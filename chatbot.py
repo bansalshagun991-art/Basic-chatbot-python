b76a312e5e4d2e99156d749b45b50e134463d9e2
bansalshagun991-art <bansalshagun991@gmail.com>
2026-05-16 12:04:59 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,24 @@
+# TASK 4: Basic Chatbot
+
+def chatbot_response(user_input):
+    user_input = user_input.lower()
+
+    if user_input == "hello":
+        return "Hi!"
+    elif user_input == "how are you":
+        return "I'm fine, thanks!"
+    elif user_input == "bye":
+        return "Goodbye!"
+    else:
+        return "Sorry, I don't understand that."
+
+print("🤖 Chatbot is running...")
+print("Type 'bye' to exit\n")
+
+while True:
+    user_input = input("You: ")
+    response = chatbot_response(user_input)
+    print("Bot:", response)
+
+    if user_input.lower() == "bye":
+        break
